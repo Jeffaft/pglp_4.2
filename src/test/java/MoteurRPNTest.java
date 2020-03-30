c6a50e9bfe4af69c23e911ef9f2231fa3c9a6c77
Julien Reynaud <jeffaft78@gmail.com>
2020-03-30 21:46:14 +0200

    derniers tests ajoutés

--- src/test/java/MoteurRPNTest.java
+++ src/test/java/MoteurRPNTest.java
@@ -28,5 +28,68 @@ public class MoteurRPNTest {
 		assertTrue(true == m.readyOp());
 	}
 	
-
+	@Test
+	public void clearSaveTest() {
+		m.addOperande(8.0);
+		assertEquals(true, m.undoStack.isEmpty());
+		m.addOperande(10.0);
+		assertTrue(8.0 == m.undoStack.pop());
+	}
+	
+	@Test
+	public void plusOpTest() {
+		m.addOperande(8.0);
+		m.addOperande(2.0);
+		m.executeCommand("+");
+		assertTrue(10.0 == m.stack.pop());
+	}
+	
+	@Test
+	public void moinsOpTest() {
+		m.addOperande(8.0);
+		m.addOperande(2.0);
+		m.executeCommand("-");
+		assertTrue(-6.0 == m.stack.pop());
+	}
+	
+	@Test
+	public void multOpTest() {
+		m.addOperande(8.0);
+		m.addOperande(2.0);
+		m.executeCommand("*");
+		assertTrue(16.0 == m.stack.pop());
+	}
+	
+	@Test
+	public void divOpTest() {
+		m.addOperande(2.0);
+		m.addOperande(16.0);
+		m.executeCommand("/");
+		assertTrue(8.0 == m.stack.pop());
+	}
+	
+	@Test
+	public void undoTest() {
+		m.addOperande(2.0);
+		m.addOperande(16.0);
+		m.executeCommand("/");
+		m.executeCommand("undo");
+		assertTrue(16.0 == m.stack.pop());
+		assertTrue(2.0 == m.stack.pop());
+		assertEquals(true, m.stack.isEmpty());
+	}
+	
+	@Test
+	public void genTest() {
+		m.addOperande(2.0);
+		m.addOperande(16.0);
+		m.executeCommand("/");
+		m.addOperande(2.0);
+		m.executeCommand("+");
+		m.addOperande(5.0);
+		m.executeCommand("*");
+		m.addOperande(60.0);
+		m.executeCommand("-");
+		assertTrue(10.0 == m.stack.pop());
+	}
 }
